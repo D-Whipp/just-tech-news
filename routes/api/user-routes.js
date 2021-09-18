e2ed9f49e92fbf6a004a009fb725fa9c19926790
Mr Whipp <dwhipp88@gmail.com>
2021-09-17 20:19:30 -0600

    refactored upvote route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const {
+  User,
+  Post,
+  Vote
+} = require("../../models");
 
 // Get /api/users
 router.get("/", (req, res) => {
   // Access our User model and run .findAll() method
   User.findAll({
-    attributes: { exclude: ["password"] },
-  })
+      attributes: {
+        exclude: ["password"]
+      },
+    })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
@@ -17,14 +23,29 @@ router.get("/", (req, res) => {
 // GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
-    attributes: { exclude: ["password"] },
-    where: {
-      id: req.params.id,
-    },
-  })
+      attributes: {
+        exclude: ["password"]
+      },
+      include: [{
+          model: Post,
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
+        }
+      ],
+      where: {
+        id: req.params.id,
+      },
+    })
     .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
+        res.status(404).json({
+          message: "No user found with this id"
+        });
         return;
       }
       res.json(dbUserData);
@@ -38,10 +59,10 @@ router.get("/:id", (req, res) => {
 // POST /api/users
 router.post("/", (req, res) => {
   User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  })
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
       console.log(err);
@@ -56,7 +77,9 @@ router.post('/login', (req, res) => {
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({
+        message: 'No user with that email address!'
+      });
       return;
     }
 
@@ -66,11 +89,16 @@ router.post('/login', (req, res) => {
     const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res.status(400).json({
+        message: 'Incorrect password!'
+      });
       return;
     }
 
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
+    res.json({
+      user: dbUserData,
+      message: 'You are now logged in!'
+    });
   });
 });
 
@@ -79,14 +107,16 @@ router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id,
-    },
-  })
+      individualHooks: true,
+      where: {
+        id: req.params.id,
+      },
+    })
     .then((dbUserData) => {
       if (!dbUserData[0]) {
-        res.status(404).json({ message: "No user found with this id" });
+        res.status(404).json({
+          message: "No user found with this id"
+        });
         return;
       }
       res.json(dbUserData);
@@ -100,13 +130,15 @@ router.put("/:id", (req, res) => {
 // DELETE /api/users/1
 router.delete("/:id", (req, res) => {
   User.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
+      where: {
+        id: req.params.id,
+      },
+    })
     .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
+        res.status(404).json({
+          message: "No user found with this id"
+        });
         return;
       }
       res.json(dbUserData);
